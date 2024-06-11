ac2a3d39de261d9727e676e793c5ced1ff2cbed0
dar3valo <wongdarren516@gmail.com>
2024-06-11 14:10:42 +0800

    Commit #2: Try to pull request

--- src/javaca1/Module.java
+++ src/javaca1/Module.java
@@ -9,8 +9,33 @@ package javaca1;
  * @author Darren
  */
 public class Module {
-    private String module_cd = moduleCode;
-    private String module_name = moduleName;
-    private int credit_units = creditUnit;
-    private double student_marks = studentMarks;
+    private String module_cd;
+    private String module_name;
+    private int credit_units;
+    private double student_marks;
+    
+    public Module(String module_cd, String module_name, int credit_units, double student_marks) {
+        this.module_cd = module_cd;
+        this.module_name = module_name;
+        this.credit_units = credit_units;
+        this.student_marks = student_marks;
+    }
+
+    public String getModule_cd() {
+        return module_cd;
+    }
+
+    public String getModule_name() {
+        return module_name;
+    }
+
+    public int getCredit_units() {
+        return credit_units;
+    }
+
+    public double getStudent_marks() {
+        return student_marks;
+    }
+    
+    
 }
---
+++ src/javaca1/Output.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaca1;
+
+/**
+ *
+ * @author Darren
+ */
+public class Output {
+    public static void main(String[] args){
+        
+    }
+}
