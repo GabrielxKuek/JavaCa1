fbd075445151e547ebb85321ead014c86a0c240a
dar3valo <wongdarren516@gmail.com>
2024-06-16 16:57:41 +0800

    Commit #19: Added backend system for login system for student enquiry system

---
+++ src/javaca1/StudentEnquirySystem.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaca1;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Darren
+ */
+public class StudentEnquirySystem {
+    private ArrayList<Student> students;
+    
+    public StudentEnquirySystem(){
+        this.students = new ArrayList<>();
+        students.add(new Student("Darren123", "1234"));
+        students.add(new Student("Rafael234", "nice"));
+    }
+    
+    public boolean authenticate(String username, String password){
+        for(Student student : students){
+            if(student.getUsername().equals(username) && 
+               student.getPassword().equals(password)){
+            return true; //Authentication is successful
+            }
+        }
+        return false; //Authentication failed
+    }
+    
+}
