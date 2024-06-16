73cbb036566097adaadf702428443e9bddf25c73
dar3valo <wongdarren516@gmail.com>
2024-06-16 16:58:17 +0800

    Commit #20: Added methods to prompt user for username and password

--- src/javaca1/StudentEnquiryView.java
+++ src/javaca1/StudentEnquiryView.java
@@ -10,12 +10,26 @@ import javax.swing.JOptionPane;
  * @author Gabriel Kuek
  */
 public class StudentEnquiryView {
-    
+
     public String showMenu(String message) {
         return JOptionPane.showInputDialog(null,
                 message,
                 "Student Enquiry System",
                 JOptionPane.QUESTION_MESSAGE
         );
+    }
+    
+    public String username(){
+        return JOptionPane.showInputDialog(null,
+                "Enter Your Username",
+                "Student Login",
+                JOptionPane.QUESTION_MESSAGE);
+    }
+    
+    public String password(){
+        return JOptionPane.showInputDialog(null,
+                "Enter Your Password",
+                "Student Login",
+                JOptionPane.QUESTION_MESSAGE);
     }
 }
