dbb15b89f51708dfe05028af8e22ee5eb68aed1d
GabrielxKuek <kuekguanjiegabriel@gmail.com>
2024-06-18 00:42:13 +0800

    fixed indentation on StudentUser file

--- src/javaca1/StudentUser.java
+++ src/javaca1/StudentUser.java
@@ -18,83 +18,83 @@ public class StudentUser {
         boolean loggedIn = false;
         
         while(true){
-        //prompt for username and password
-        if(!loggedIn){
-        String username = view.username();
-        String password = view.password();
+            //prompt for username and password
+            if(!loggedIn){
+            String username = view.username();
+            String password = view.password();
         
-        //Authenticate User
-        if(enquirySystem.authenticate(username, password)){
-            JOptionPane.showMessageDialog(null,
-                    "Login Successful!",
-                    "Success!",
-                    JOptionPane.INFORMATION_MESSAGE);
-            loggedIn = true;
-        }else{
-            JOptionPane.showMessageDialog(null,
-                    "Incorrect Credentials",
+            // Authenticate User
+                if(enquirySystem.authenticate(username, password)){
+                    JOptionPane.showMessageDialog(null,
+                            "Login Successful!",
+                            "Success!",
+                            JOptionPane.INFORMATION_MESSAGE);
+                    loggedIn = true;
+                }else{
+                    JOptionPane.showMessageDialog(null,
+                            "Incorrect Credentials",
+                            "Error",
+                            JOptionPane.ERROR_MESSAGE);
+                    continue;
+                }
+            }
+            
+            // JOptionPane Menu
+            String userInputString = view.showMenu("""
+                                                   Enter your option: 
+
+                                                   1. Display All Students: 
+                                                   2.Search student by class
+                                                   3. Search student by name
+                                                   4. Quit""");
+
+
+            int userInput;
+
+            try {
+                userInput = Integer.parseInt(userInputString.trim());
+            } catch (NumberFormatException e) {
+                // Handle non-integer input
+                JOptionPane.showMessageDialog(null,
+                    "Please enter a valid integer option",
                     "Error",
                     JOptionPane.ERROR_MESSAGE);
-            continue;
-        }
-    }
-            
-        // JOptionPane Menu
-        String userInputString = view.showMenu("""
-                                               Enter your option: 
-                                               
-                                               1. Display All Students: 
-                                               2.Search student by class
-                                               3. Search student by name
-                                               4. Quit""");
-        
-        
-        int userInput;
-        
-        try {
-            userInput = Integer.parseInt(userInputString.trim());
-        } catch (NumberFormatException e) {
-            // Handle non-integer input
-            JOptionPane.showMessageDialog(null,
-                "Please enter a valid integer option",
-                "Error",
-                JOptionPane.ERROR_MESSAGE);
-            continue; // Restart the loop to prompt for input again
-        }
-         
-        // call student management
-        StudentManagement options = new StudentManagement();
-        
-        switch(userInput){
-            case 1:
-                options.display_student();
-                break;
-            case 2:
-                //Prompt user for classInput
-                String classInput = JOptionPane.showInputDialog(null,
-                "Enter the class name to search",
-                "Search",
-                JOptionPane.QUESTION_MESSAGE
-                );                
-                options.search_studentClass(classInput);
-                break;
-            case 3:
-                options.search_studentName();
-                break;
-            case 4:
-                JOptionPane.showMessageDialog(null,
-                        "Program Terminated.\nThank you!",
-                        "Message",
-                        JOptionPane.INFORMATION_MESSAGE
-                );
-                return;
-            default:
-                JOptionPane.showMessageDialog(null,
-                "Please provide a valid input",
-                "Error",
-                JOptionPane.ERROR_MESSAGE
-                );
+                continue; // Restart the loop to prompt for input again
             }
-        }        
+
+            // call student management
+            StudentManagement studentManagement = new StudentManagement();
+
+            switch(userInput){
+                case 1:
+                    studentManagement.display_student();
+                    break;
+                case 2:
+                    //Prompt user for classInput
+                    String classInput = JOptionPane.showInputDialog(null,
+                    "Enter the class name to search",
+                    "Search",
+                    JOptionPane.QUESTION_MESSAGE
+                    );                
+                    studentManagement.search_studentClass(classInput);
+                    break;
+                case 3:
+                    studentManagement.search_studentName();
+                    break;
+                case 4:
+                    JOptionPane.showMessageDialog(null,
+                            "Program Terminated.\nThank you!",
+                            "Message",
+                            JOptionPane.INFORMATION_MESSAGE
+                    );
+                    return;
+                default:
+                    JOptionPane.showMessageDialog(null,
+                    "Please provide a valid input",
+                    "Error",
+                    JOptionPane.ERROR_MESSAGE
+                    );
+            }
+        }      
     }
 }
