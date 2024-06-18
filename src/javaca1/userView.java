cdda69c20cc22e84b431fc1b318062dd5e8c9b5f
dar3valo <wongdarren516@gmail.com>
2024-06-18 13:04:43 +0800

    Commit #24: Changed from information message to error message for displaying message if the user is illegal or non existent

--- src/javaca1/userView.java
+++ src/javaca1/userView.java
@@ -17,21 +17,17 @@ public class userView {
     private static User tempUser = new User();
     public static boolean isMenuRepeat = true;
     
-    // main program
-    public static void main(String[] args) {
-        displayMenu();
-    }
     
     // called when displaying login system
     public static void displayMenu() {
         while (isMenuRepeat) {
             userInputUsername = JOptionPane.showInputDialog(null,
-                    "Enter your username:",
+                    "Enter your username",
                     programName,
                     JOptionPane.QUESTION_MESSAGE);
 
             userInputPassword = JOptionPane.showInputDialog(null,
-                    "Enter your password:",
+                    "Enter your password",
                     programName,
                     JOptionPane.QUESTION_MESSAGE);
 
@@ -52,7 +48,7 @@ public class userView {
                 JOptionPane.showMessageDialog(null,
                     "Incorrect credentials. Please try again!",
                     "Success!",
-                    JOptionPane.INFORMATION_MESSAGE);
+                    JOptionPane.ERROR_MESSAGE);
                         
                 continue;
             }
@@ -68,34 +64,5 @@ public class userView {
 
             }
         }
-//        switch (userController.checkUserType(tempUser)) {
-//            case true:
-//                StudentEnquiryView.showMenu();
-//        }
-//        StudentEnquirySystem enquirySystem = new StudentEnquirySystem();
-//        StudentEnquiryView view = new StudentEnquiryView();
-//        boolean loggedIn = false;
-//        
-//        while(true){
-//            //prompt for username and password
-//            if(!loggedIn){
-//            String username = view.username();
-//            String password = view.password();
-//        
-//            // Authenticate User
-//                if(enquirySystem.authenticate(username, password)){
-//                    JOptionPane.showMessageDialog(null,
-//                            "Login Successful!",
-//                            "Success!",
-//                            JOptionPane.INFORMATION_MESSAGE);
-//                    loggedIn = true;
-//                }else{
-//                    JOptionPane.showMessageDialog(null,
-//                            "Incorrect Credentials",
-//                            "Error",
-//                            JOptionPane.ERROR_MESSAGE);
-//                    continue;
-//                }
-//            }
     }
 }
