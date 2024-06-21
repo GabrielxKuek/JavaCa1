01380dd5f8c644338c722744ee1f2ab7c43e666f
dar3valo <wongdarren516@gmail.com>
2024-06-21 12:47:52 +0800

    Commit #32: Removed unnecessary comments

--- src/javaca1/Student.java
+++ src/javaca1/Student.java
@@ -15,13 +15,6 @@ public class Student {
     private Module[] modules_taken;
     private String username;
     private String password;
-    
-//    public Module[] modules_taken = {
-//        new Module("ST0405", "GabrielStudys", 4, 0.0),
-//        new Module("ST0504", "DarrenStudys", 3, 0.0),
-//        new Module("ST0690", "KevinGayStudys", 3, 0.0),
-//        new Module("ST0420", "RafaelHonestStudys", 4, 0.0)
-//    };
 
     // student class constructors
     public Student(String name, String adminNo, String classes, Module[] modules) {
@@ -94,14 +87,6 @@ public class Student {
             new Module("ST0503", "FOP", 5, 0.0)
         }; // sample dataset. later change
     }
-    
-//    public void printDetails() {
-//        System.out.println("Student Details:");
-//        System.out.println("Name: " + name);
-//        System.out.println("Age: " + adminNo);
-//        System.out.println("Class: " + classes);
-//        System.out.println("Module taken: " + modules_taken);
-//    }  
     
     public double calculateGPA() {
         // config
