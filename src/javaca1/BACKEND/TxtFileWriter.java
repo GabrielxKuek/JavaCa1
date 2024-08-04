d54cc000f41795b5d594816d22627d6aeed6f7b0
GabrielxKuek <kuekguanjiegabriel@gmail.com>
2024-08-04 22:11:35 +0800

    init writer file

---
+++ src/javaca1/BACKEND/TxtFileWriter.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaca1.BACKEND;
+
+/**
+ *
+ * @author Gabriel Kuek
+ */
+
+// imports
+import java.io.BufferedWriter;
+
+public class TxtFileWriter {
+    // receives input from form
+    
+    // updates student array
+    
+    // rewrite file with file writer
+}
